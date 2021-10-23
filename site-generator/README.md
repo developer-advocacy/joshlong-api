48d697c5d7df931e6c169e663db73f6103413f81
Josh Long <josh@joshlong.com>
2021-10-22 17:38:00 -0700

    site-generator

---
+++ site-generator/README.md
@@ -0,0 +1,3 @@
+# Blog 
+1. given the content root, generate a parallel hierarchy of pages to feed into a static site generator 
+2. 
---
+++ site-generator/src/test/java/com/joshlong/sitegenerator/SiteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshlong.sitegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
