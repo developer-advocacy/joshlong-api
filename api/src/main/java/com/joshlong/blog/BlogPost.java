498947589258240f8520daff88573080a0d56e90
Josh Long <josh@joshlong.com>
2021-12-31 19:51:21 -0800

    up

--- api/src/main/java/com/joshlong/blog/BlogPost.java
+++ api/src/main/java/com/joshlong/blog/BlogPost.java
@@ -7,5 +7,7 @@ import java.util.List;
  * Meant to describe posts as they exist in the database
  */
 public record BlogPost(String title, Date date, String originalContent, String processedContent, boolean published,
-		BlogPostContentType type, String path, /* this is the unique bit without the /jl/blogPost/ */ String pathId, List<String> images, List<String> paragraphs) {
+		BlogPostContentType type, String path,
+		/* this is the unique bit without the /jl/blogPost/ */ String pathId, List<String> images,
+		List<String> paragraphs) {
 }
--- api/src/main/java/com/joshlong/blog/blogs/DefaultBlogPostService.java
+++ api/src/main/java/com/joshlong/blog/blogs/DefaultBlogPostService.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.springframework.util.Assert;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StringUtils;
 
 import java.io.*;
 import java.text.DateFormat;
@@ -18,98 +19,131 @@ import java.util.stream.Collectors;
 @Slf4j
 class DefaultBlogPostService implements BlogPostService {
 
-    private final MarkdownService markdownService;
+	private final MarkdownService markdownService;
 
-    private final DateFormat simpleDateFormat;
+	private final DateFormat simpleDateFormat;
 
-    DefaultBlogPostService(MarkdownService markdownService, DateFormat simpleDateFormat) {
-        this.markdownService = markdownService;
-        this.simpleDateFormat = simpleDateFormat;
-    }
+	DefaultBlogPostService(MarkdownService markdownService, DateFormat simpleDateFormat) {
+		this.markdownService = markdownService;
+		this.simpleDateFormat = simpleDateFormat;
+	}
 
-    @SneakyThrows
-    private BlogPost buildBlogPostFrom(BlogPostContentType type, String path, InputStream file) {
-        try (var f = new InputStreamReader(file)) {
-            var contents = FileCopyUtils.copyToString(f);
-            return buildBlogPostFrom(type, path, contents);
-        }
-    }
+	@SneakyThrows
+	private BlogPost buildBlogPostFrom(BlogPostContentType type, String path, InputStream file) {
+		try (var f = new InputStreamReader(file)) {
+			var contents = FileCopyUtils.copyToString(f);
+			return buildBlogPostFrom(type, path, contents);
+		}
+	}
 
-    @SneakyThrows
-    @Override
-    public BlogPost buildBlogPostFrom(String path, File file) {
-        if (log.isDebugEnabled()) {
-            log.debug("------");
-            log.debug("indexing " + file.getAbsolutePath() + " with path " + path);
-        }
-        Assert.notNull(file, () -> "the file must not be null");
-        Assert.state(file.exists(), () -> "the file " + file.getAbsolutePath() + " does not exist!");
-        var type = file.getName().toLowerCase(Locale.ROOT).endsWith(".md") ? BlogPostContentType.MD
-                : BlogPostContentType.HTML;
-        return buildBlogPostFrom(type, path, new FileInputStream(file));
-    }
+	@SneakyThrows
+	@Override
+	public BlogPost buildBlogPostFrom(String path, File file) {
+		if (log.isDebugEnabled()) {
+			log.debug("------");
+			log.debug("indexing " + file.getAbsolutePath() + " with path " + path);
+		}
+		Assert.notNull(file, () -> "the file must not be null");
+		Assert.state(file.exists(), () -> "the file " + file.getAbsolutePath() + " does not exist!");
+		var type = file.getName().toLowerCase(Locale.ROOT).endsWith(".md") ? BlogPostContentType.MD
+				: BlogPostContentType.HTML;
+		return buildBlogPostFrom(type, path, new FileInputStream(file));
+	}
 
-    /*
-     * Given the HTML, we can use JSoup to discover the source attributes for all images
-     * in the markup
-     */
-    private List<String> discoverImages(String html) {
-        var results = new ArrayList<String>();
-        var document = Jsoup.parse(html);
-        var images = document.getElementsByTag("img");
-        if (images != null && images.size() > 0) {
-            images.forEach(element -> {
-                if (element.hasAttr("src"))
-                    results.add(element.attr("src"));
-            });
-        }
-        return results;
-    }
+	/*
+	 * Given the HTML, we can use JSoup to discover the source attributes for all images
+	 * in the markup
+	 */
+	private List<String> discoverImages(String html) {
+		var results = new ArrayList<String>();
+		var document = Jsoup.parse(html);
+		var images = document.getElementsByTag("img");
+		if (images != null && images.size() > 0) {
+			images.forEach(element -> {
+				if (element.hasAttr("src"))
+					results.add(element.attr("src"));
+			});
+		}
+		return results;
+	}
 
-    private List<String> discoverPreviewParagraphs(String html, int countOfParagraphs) {
-        var document = Jsoup.parse(html);
-        var ps = document.getElementsByTag("p");
-        var results = new ArrayList<String>();
-        if (ps != null && ps.size() > 0) {
-            ps.forEach(element -> results.add(element.text()));
-        }
-        return results.stream().limit(countOfParagraphs).collect(Collectors.toList());
-    }
+	/**
+	 * The images used to be hosted on joshlong.com/media/*, but now they're served off of
+	 * some other URI, like api.joshlong.com/media, or maybe some other port. Either way,
+	 * we need to make sure that the images are correctly resolved in the new world.
+	 * @param resolveMediaServerUri the new API endpoint serving the responses
+	 * @param localImagePrefixToMatch the pattern we will match in the HTML markup to
+	 * detect old file paths
+	 * @param html the HTML contents we want to change
+	 * @return the resolved images
+	 */
+	private String resolveImageSources(String resolveMediaServerUri, String localImagePrefixToMatch, String html) {
 
-    @SneakyThrows
-    private BlogPost buildBlogPostFrom(BlogPostContentType type, String path, String contents) {
-        var headerDivider = "~~~~~~";
-        Assert.state(contents.contains(headerDivider), () -> "this blog  does not contain any headers! " + contents);
-        var parts = contents.split(headerDivider);
-        var header = buildHeader(parts[0]);
-        var dateFromHeaderString = header.get("date");
-        Assert.notNull(dateFromHeaderString, () -> "the blog must have a published date!");
-        var date = buildHeaderDate(dateFromHeaderString);
-        var processedContent = this.markdownService.convertMarkdownTemplateToHtml(parts[1]);
-        var published = header.get("status").toLowerCase(Locale.ROOT).equalsIgnoreCase("published");
-        var images = discoverImages(processedContent);
-        var firstParagraph = discoverPreviewParagraphs(processedContent, 2);
-        var uniquePath = path.toLowerCase(Locale.ROOT).startsWith("/jl/blogpost/") ?   path.substring("/jl/blogpost/".length()) : path ;
-        return new BlogPost(header.get("title"), date, contents, processedContent, published, type, path, uniquePath, images,
-                firstParagraph);
-    }
+		if (resolveMediaServerUri.endsWith("/"))
+			resolveMediaServerUri = resolveMediaServerUri.substring(0, resolveMediaServerUri.length() - 1);
 
-    @SneakyThrows
-    private Date buildHeaderDate(String date) {
-        Assert.state(3 == date.split("-").length, () -> "there should be 3 parts to the date");
-        return this.simpleDateFormat.parse(date);
-    }
+		var newHtml = html;
+		var docuemnt = Jsoup.parse(html);
+		var imgs = docuemnt.getElementsByTag("img");
+		for (var i : imgs) {
+			var ogSrc = i.attr("src");
+			var src = (ogSrc + "").trim();
+			log.info("the src is " + src);
+			if (src.startsWith(localImagePrefixToMatch)) {
+				var newSrc = resolveMediaServerUri + src;
+				newHtml = StringUtils.replace(newHtml, ogSrc, newSrc);
+			}
+		}
+		return newHtml;
+	}
 
-    @SneakyThrows
-    private Map<String, String> buildHeader(String header) {
-        try (var sw = new StringReader(header)) {
-            var props = new Properties();
-            props.load(sw);
-            var map = new HashMap<String, String>();
-            for (var key : props.keySet())
-                map.put((String) key, (props.getProperty((String) key) + "").trim());
-            return map;
-        }
-    }
+	private List<String> discoverPreviewParagraphs(String html, int countOfParagraphs) {
+		var document = Jsoup.parse(html);
+		var ps = document.getElementsByTag("p");
+		var results = new ArrayList<String>();
+		if (ps != null && ps.size() > 0) {
+			ps.forEach(element -> results.add(element.text()));
+		}
+		return results.stream().limit(countOfParagraphs).collect(Collectors.toList());
+	}
+
+	@SneakyThrows
+	private BlogPost buildBlogPostFrom(BlogPostContentType type, String path, String contents) {
+		var headerDivider = "~~~~~~";
+
+		Assert.state(contents.contains(headerDivider), () -> "this blog  does not contain any headers! " + contents);
+		var parts = contents.split(headerDivider);
+		var header = buildHeader(parts[0]);
+		var dateFromHeaderString = header.get("date");
+		Assert.notNull(dateFromHeaderString, () -> "the blog must have a published date!");
+		var date = buildHeaderDate(dateFromHeaderString);
+		var processedContent = this.markdownService.convertMarkdownTemplateToHtml(parts[1]);
+		processedContent = resolveImageSources("http://localhost:8080/", "/media/", processedContent);
+		var published = header.get("status").toLowerCase(Locale.ROOT).equalsIgnoreCase("published");
+		var images = discoverImages(processedContent);
+		var firstParagraph = discoverPreviewParagraphs(processedContent, 2);
+		var uniquePath = path.toLowerCase(Locale.ROOT).startsWith("/jl/blogpost/")
+				? path.substring("/jl/blogpost/".length()) : path;
+		return new BlogPost(header.get("title"), date, contents, processedContent, published, type, path, uniquePath,
+				images, firstParagraph);
+	}
+
+	@SneakyThrows
+	private Date buildHeaderDate(String date) {
+		Assert.state(3 == date.split("-").length, () -> "there should be 3 parts to the date");
+		return this.simpleDateFormat.parse(date);
+	}
+
+	@SneakyThrows
+	private Map<String, String> buildHeader(String header) {
+		try (var sw = new StringReader(header)) {
+			var props = new Properties();
+			props.load(sw);
+			var map = new HashMap<String, String>();
+			for (var key : props.keySet())
+				map.put((String) key, (props.getProperty((String) key) + "").trim());
+			return map;
+		}
+	}
 
 }
