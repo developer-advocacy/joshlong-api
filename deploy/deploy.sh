77f6facb8c0a373ff877e03135e3711f6bb05818
Josh Long <josh@joshlong.com>
2022-01-01 23:54:36 -0800

    up

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,7 @@ set -o pipefail
 APP_NAME=joshlong-com-api
 IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 docker rmi -f $IMAGE_NAME
+cd $ROOT_DIR
 ./mvnw -DskipTests=true spring-javaformat:apply clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
 docker push $IMAGE_NAME
 export RESERVED_IP_NAME=${APP_NAME}-ip
