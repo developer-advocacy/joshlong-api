b1657c08f985beb1d8cb51edaafe510fd6170849
Josh Long <josh@joshlong.com>
2022-01-01 23:29:40 -0800

    deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+APP_NAME=joshlong-api
+IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
+docker rmi -f $IMAGE_NAME
+./mvnw -DskipTests=true spring-javaformat:apply clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+docker push $IMAGE_NAME
+export RESERVED_IP_NAME=${APP_NAME}-ip
+gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME || gcloud compute addresses create $RESERVED_IP_NAME --global
+kubectl apply -f $ROOT_DIR/deploy/k8s
+
