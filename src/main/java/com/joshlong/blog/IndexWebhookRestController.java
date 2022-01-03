57c3b32e601b3f2786acb4f2c063f9cfe3dff6cd
Josh Long <josh@joshlong.com>
2022-01-02 19:34:37 -0800

    up

--- src/main/java/com/joshlong/blog/IndexWebhookRestController.java
+++ src/main/java/com/joshlong/blog/IndexWebhookRestController.java
@@ -32,6 +32,9 @@ class IndexWebhookRestController {
 	IndexWebhookRestController(IndexService indexService, BlogProperties properties) {
 		this.indexService = indexService;
 		this.properties = properties;
+
+		log.info(System.getenv("BLOG_INDEX_REBUILD_KEY"));
+		log.info(this.properties.indexRebuildKey());
 	}
 
 	@PostMapping("/index")
