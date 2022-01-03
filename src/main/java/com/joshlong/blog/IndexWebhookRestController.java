c53039b6071499bc1c83fe99ae95ea2b7032c58e
Josh Long <josh@joshlong.com>
2022-01-02 19:09:34 -0800

    up

--- src/main/java/com/joshlong/blog/IndexWebhookRestController.java
+++ src/main/java/com/joshlong/blog/IndexWebhookRestController.java
@@ -1,10 +1,10 @@
 package com.joshlong.blog;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,76 +31,76 @@ class IndexWebhookRestController {
 
 	private final String computedKey;
 
-	IndexWebhookRestController(IndexService indexService, BlogProperties properties) throws Exception {
+	IndexWebhookRestController(IndexService indexService, BlogProperties properties) {
 		this.indexService = indexService;
 		this.properties = properties;
-		this.computedKey = this.deriveKey();
-		log.info("the computed key is " + this.computedKey);
-	}
-
-	@SneakyThrows
-	private String deriveKey() {
-		return (this.properties.indexRebuildKey());
-
+		this.computedKey = this.properties.indexRebuildKey();
 	}
 
 	@PostMapping("/index")
 	ResponseEntity<?> refresh(RequestEntity<String> requestEntity) throws Exception {
 		var secret = this.properties.indexRebuildKey();
-		var hash = HmacUtils.generateHmac256(Objects.requireNonNull(requestEntity.getBody()), secret.getBytes());
-		log.info("the hash is [" + hash + "]");
+		var hashOfTheRequestWithMyCopyOfTheSecret = HmacUtils
+				.generateHmac256(Objects.requireNonNull(requestEntity.getBody()), secret.getBytes());
+		var hashOfTheRequestWithTheirCopyOfTheSecret = getGithubWebhookRequestSha256HeaderValue(requestEntity);
+
+		log.info("mine: " + hashOfTheRequestWithMyCopyOfTheSecret);
+		log.info("theirs: " + hashOfTheRequestWithTheirCopyOfTheSecret);
+
+		if (StringUtils.hasText(hashOfTheRequestWithTheirCopyOfTheSecret)
+				&& StringUtils.hasText(hashOfTheRequestWithMyCopyOfTheSecret)) {
+			if (hashOfTheRequestWithTheirCopyOfTheSecret.contains(hashOfTheRequestWithMyCopyOfTheSecret))
+				return ResponseEntity.ok(indexService.rebuildIndex());
+
+		}
+
+		return ResponseEntity.badRequest().build();
+	}
+
+	private String getGithubWebhookRequestSha256HeaderValue(RequestEntity<String> requestEntity) {
 		var headers = requestEntity.getHeaders();
 		headers.forEach((k, v) -> log.info(k + "=" + v));
 		var headerKey = "X-Hub-Signature-256";
 		if (headers.containsKey(headerKey)) {
 			var strings = headers.get(headerKey);
 			if (Objects.requireNonNull(strings).size() > 0) {
-				var key = strings.get(0);
-				if (key.contains(this.deriveKey())) {
-					log.info("rebuilding index successfully");
-					return ResponseEntity.ok(this.indexService.rebuildIndex());
-				} //
-				else {
-					log.info("the key " + hash + " is not within " + key);
-				}
+				return strings.get(0).trim();
 			}
 		}
-		return ResponseEntity.badRequest().build();
+		return null;
 	}
 
 }
 
+/**
+ * This was taken from <a href
+ * ="https://www.javacodemonk.com/create-hmacsha256-signature-in-java-3421c36d">this blog
+ * post</a>. Thanks, JavaCodeMonk.com!
+ */
 abstract class HmacUtils {
 
 	public static String generateHmac256(String message, byte[] key)
 			throws InvalidKeyException, NoSuchAlgorithmException {
-		byte[] bytes = hmac("HmacSHA256", key, message.getBytes());
+		var bytes = hmac("HmacSHA256", key, message.getBytes());
 		return bytesToHex(bytes);
 	}
 
 	private static byte[] hmac(String algorithm, byte[] key, byte[] message)
 			throws NoSuchAlgorithmException, InvalidKeyException {
-		Mac mac = Mac.getInstance(algorithm);
+		var mac = Mac.getInstance(algorithm);
 		mac.init(new SecretKeySpec(key, algorithm));
 		return mac.doFinal(message);
 	}
 
 	private static String bytesToHex(byte[] bytes) {
-		final char[] hexArray = "0123456789abcdef".toCharArray();
-		char[] hexChars = new char[bytes.length * 2];
+		var hexArray = "0123456789abcdef".toCharArray();
+		var hexChars = new char[bytes.length * 2];
 		for (int j = 0, v; j < bytes.length; j++) {
 			v = bytes[j] & 0xFF;
 			hexChars[j * 2] = hexArray[v >>> 4];
 			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
 		}
 		return new String(hexChars);
-	}
-
-	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
-		var valueToDigest = "The quick brown fox jumps over the lazy dog";
-		var key = "1234".getBytes();
-		var messageDigest = HmacUtils.generateHmac256(valueToDigest, key);
-		System.out.println(messageDigest);
 	}
 
 }
