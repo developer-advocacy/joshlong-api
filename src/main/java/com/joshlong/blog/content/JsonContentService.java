56c69d197d8c84a582e123df985b3a17d10e7ba4
Josh Long <josh@joshlong.com>
2022-01-30 01:25:22 -0800

    up

--- src/main/java/com/joshlong/blog/content/JsonContentService.java
+++ src/main/java/com/joshlong/blog/content/JsonContentService.java
@@ -23,57 +23,57 @@ import java.util.function.Function;
 @Slf4j
 class JsonContentService implements ContentService<Collection<Content>> {
 
-    private final Function<String, String> htmlRefResolver;
+	private final Function<String, String> htmlRefResolver;
 
-    private final Resource resource;
+	private final Resource resource;
 
-    private final ObjectMapper objectMapper;
+	private final ObjectMapper objectMapper;
 
-    private final List<Content> contents = new CopyOnWriteArrayList<>();
+	private final List<Content> contents = new CopyOnWriteArrayList<>();
 
-    JsonContentService(Resource resource, Function<String, String> htmlRefResolver, ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-        this.resource = resource;
-        this.htmlRefResolver = htmlRefResolver;
-    }
+	JsonContentService(Resource resource, Function<String, String> htmlRefResolver, ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+		this.resource = resource;
+		this.htmlRefResolver = htmlRefResolver;
+	}
 
-    @Override
-    public Collection<Content> getContent() {
-        return this.contents;
-    }
+	@Override
+	public Collection<Content> getContent() {
+		return this.contents;
+	}
 
-    @SneakyThrows
-    private URL buildUrlFrom(String url) {
-        if (null == url)
-            return null;
-        return new URL(url);
-    }
+	@SneakyThrows
+	private URL buildUrlFrom(String url) {
+		if (null == url)
+			return null;
+		return new URL(url);
+	}
 
-    @EventListener(IndexingFinishedEvent.class)
-    public void indexed() throws Exception {
-        log.info("building " + getClass().getName() + " for file " + this.resource.getFile().getAbsolutePath() + '.');
-        var file = this.resource.getFile();
-        var values = this.objectMapper.readValue(file, new TypeReference<Collection<JsonNode>>() {
-        });
-        var content = values //
-                .stream()//
-                .map(json -> { //
-                    var title = JsonUtils.valueOrNull(json, "title");
-                    var html = JsonUtils.valueOrNull(json, "html");
-                    var htmlRef = JsonUtils.valueOrNull(json, "htmlRef");
-                    Assert.isTrue(StringUtils.hasText(html) || StringUtils.hasText(htmlRef),
-                            "you must provide an HTML description or a key which the HTML may be resolved");
-                    html = (!StringUtils.hasText(html)) ? this.htmlRefResolver.apply(htmlRef) : html;
-                    var imageUrl = buildUrlFrom(JsonUtils.valueOrNull(json, "imageUrl"));
-                    return new Content(title, html, imageUrl);
-                }). //
-                        toList();
+	@EventListener(IndexingFinishedEvent.class)
+	public void indexed() throws Exception {
+		log.info("building " + getClass().getName() + " for file " + this.resource.getFile().getAbsolutePath() + '.');
+		var file = this.resource.getFile();
+		var values = this.objectMapper.readValue(file, new TypeReference<Collection<JsonNode>>() {
+		});
+		var content = values //
+				.stream()//
+				.map(json -> { //
+					var title = JsonUtils.valueOrNull(json, "title");
+					var html = JsonUtils.valueOrNull(json, "html");
+					var htmlRef = JsonUtils.valueOrNull(json, "htmlRef");
+					Assert.isTrue(StringUtils.hasText(html) || StringUtils.hasText(htmlRef),
+							"you must provide an HTML description or a key which the HTML may be resolved");
+					html = (!StringUtils.hasText(html)) ? this.htmlRefResolver.apply(htmlRef) : html;
+					var imageUrl = buildUrlFrom(JsonUtils.valueOrNull(json, "imageUrl"));
+					return new Content(title, html, imageUrl);
+				}). //
+				toList();
 
-        synchronized (this.contents) {
-            this.contents.clear();
-            this.contents.addAll(content);
-        }
+		synchronized (this.contents) {
+			this.contents.clear();
+			this.contents.addAll(content);
+		}
 
-    }
+	}
 
 }
