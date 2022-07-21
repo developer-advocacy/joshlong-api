220691e96d934f09a85d548f1bf29201cdd3f068
Josh Long <josh@joshlong.com>
2022-07-20 18:52:47 -0700

    up

--- src/main/java/com/joshlong/blog/SiteUpdatedEvent.java
+++ src/main/java/com/joshlong/blog/SiteUpdatedEvent.java
@@ -2,6 +2,8 @@ package com.joshlong.blog;
 
 import org.springframework.context.ApplicationEvent;
 
+import java.util.Date;
+
 /**
  * Whenever we need to rebuild the index based on an updated git repository, we will
  * publish this event.
@@ -9,7 +11,7 @@ import org.springframework.context.ApplicationEvent;
 public class SiteUpdatedEvent extends ApplicationEvent {
 
 	public SiteUpdatedEvent() {
-		super(null);
+		super(new Date());
 	}
 
 }
